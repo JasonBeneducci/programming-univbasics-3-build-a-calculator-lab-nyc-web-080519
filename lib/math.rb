e061f1c8046c5ce81074a062285b693cbd46ec35
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-07-29 15:14:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  minus = num1- num2
+  puts minus
 end
 
 def division(num1, num2)
-
+  quotient = (num1 / num2)
+  puts quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
 end
 
 def modulo(num1, num2)
-
+  number = num1 % num2
+  puts number
 end
 
 def square_root(num)
-
+  root = num ** 0.5
 end
